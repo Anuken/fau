c8be2452ec7220fe6b60eebfa21ba857b7144ea7
Anuken <arnukren@gmail.com>
2020-06-25 16:51:27 -0400

    stuff

--- test/testing.nim
+++ test/testing.nim
@@ -34,7 +34,8 @@ makeSystem("render", [Pos, Render]):
     randomize()
 
     for i in 0..1000:
-      discard newEntityWith(Render(), Pos(x: rand(-500..500).float32, y: rand(-500..500).float32), Bouncer(vel: vec2(rand(-10..10).float32, rand(-10..10).float32)))
+      let e = newEntityWith(Render(), Pos(x: rand(-500..500).float32, y: rand(-500..500).float32), Bouncer(vel: vec2(rand(-10..10).float32, rand(-10..10).float32)))
+      if i == 0: echo e
 
   start:
     if keyEscape.tapped: quitApp()
