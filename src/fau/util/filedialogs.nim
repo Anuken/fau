30a7354bdcb6c07eaa1eebca7622b2f53794e36f
Anuken <arnukren@gmail.com>
2024-08-23 06:54:56 -0400

    Potential fix for Windows file dialogs

--- src/fau/util/filedialogs.nim
+++ src/fau/util/filedialogs.nim
@@ -1,18 +1,33 @@
 import tinyfd/tinyfd
 
-proc saveFileDialog*(title = "Save File", defaultPathAndFile = "", patterns: seq[string] = @["*.*"], filterDescription = "All Files"): string =
-  var pats = allocCStringArray(patterns)
-  
-  result = $tinyfd_saveFileDialog(title.cstring, defaultPathAndFile.cstring, patterns.len.cint, pats, filterDescription.cstring)
+when defined(Windows):
+  import sequtils
 
-  pats.deallocCStringArray
+proc saveFileDialog*(title = "Save File", defaultPathAndFile = "", patterns: seq[string] = @["*.*"], filterDescription = "All Files"): string =
+  var patList = patterns
+  #does not work properly on macos
+  when defined(macosx): patList.insert("", 0)
+  
+  when defined(Windows):
+    var pats = patList.mapIt(it.newWideCString)
+    result = $tinyfd_saveFileDialogW(title.newWideCString, defaultPathAndFile.newWideCString, pats.len.cint, cast[ptr UncheckedArray[WideCString]](pats[0]), filterDescription.newWideCString)
+  else:
+    var pats = allocCStringArray(patList)
+    result = $tinyfd_saveFileDialog(title.cstring, defaultPathAndFile.cstring, patList.len.cint, pats, filterDescription.cstring)
+    pats.deallocCStringArray
 
 proc openFileDialog*(title = "Open File", defaultPathAndFile = "", patterns: seq[string] = @["*.*"], filterDescription = "All Files", multiSelect = false): string =
-  var pats = allocCStringArray(patterns)
+  var patList = patterns
+  #does not work properly on macos
+  when defined(macosx): patList.insert("", 0)
   
-  result = $tinyfd_openFileDialog(title.cstring, defaultPathAndFile.cstring, patterns.len.cint, pats, filterDescription.cstring, multiSelect.cint)
-
-  pats.deallocCStringArray
+  when defined(Windows):
+    var pats = patList.mapIt(it.newWideCString)
+    result = $tinyfd_openFileDialogW(title.newWideCString, defaultPathAndFile.newWideCString, pats.len.cint, cast[ptr UncheckedArray[WideCString]](pats[0]), filterDescription.newWideCString, multiSelect.cint)
+  else:
+    var pats = allocCStringArray(patList)
+    result = $tinyfd_openFileDialog(title.cstring, defaultPathAndFile.cstring, patList.len.cint, pats, filterDescription.cstring, multiSelect.cint)
+    pats.deallocCStringArray
 
 when isMainModule:
   var patterns = @["*.png", "*.msav"]
--- src/fau/util/tinyfd/tinyfd.nim
+++ src/fau/util/tinyfd/tinyfd.nim
@@ -12,5 +12,9 @@ proc currentSourceDir(): string {.compileTime.} =
 {.passC: "-I" & currentSourceDir().}
 
 proc tinyfd_saveFileDialog*(title, defaultPathAndFile: cstring, numOfFilterPatterns: cint, filterPatterns: cstringArray, singleFilterDescription: cstring): cstring {.cdecl, header: "tinyfiledialogs.h".}
-
 proc tinyfd_openFileDialog*(title, defaultPathAndFile: cstring, numOfFilterPatterns: cint, filterPatterns: cstringArray, singleFilterDescription: cstring, aAllowMultipleSelects: cint): cstring {.cdecl, header: "tinyfiledialogs.h".}
+
+when defined(Windows):
+  
+  proc tinyfd_saveFileDialogW*(title, defaultPathAndFile: WideCString, numOfFilterPatterns: cint, filterPatterns: ptr UncheckedArray[WideCString], singleFilterDescription: WideCString): WideCString {.cdecl, header: "tinyfiledialogs.h".}
+  proc tinyfd_openFileDialogW*(title, defaultPathAndFile: WideCString, numOfFilterPatterns: cint, filterPatterns: ptr UncheckedArray[WideCString], singleFilterDescription: WideCString, aAllowMultipleSelects: cint): WideCString {.cdecl, header: "tinyfiledialogs.h".}
