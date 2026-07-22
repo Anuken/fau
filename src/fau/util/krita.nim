e95327519fcb8430454b5e3d8a063646bbf59929
Anuken <arnukren@gmail.com>
2026-07-22 11:17:40 -0400

    krita clone layer support

--- src/fau/util/krita.nim
+++ src/fau/util/krita.nim
@@ -1,4 +1,4 @@
-import pkg/zippy/ziparchives, pkg/nimlzf, std/[streams, xmlparser, xmltree, strutils]
+import pkg/zippy/ziparchives, pkg/nimlzf, std/[streams, xmlparser, xmltree, strutils, tables]
 
 ## Utility for reading Krita files - Guidance taken from https://krita-artists.org/t/what-image-format-do-kra-files-use-for-layers/91242/3 and https://invent.kde.org/documentation/docs-krita-org/-/merge_requests/105/diffs
 ## Currently only supports reading Krita files from disk due to zippy limitations (https://github.com/guzba/zippy/issues/56)
@@ -13,10 +13,12 @@ type KritaLayer* = ref object
   visible*: bool
   passthrough*: bool
   locked*: bool
+  collapsed*: bool
   name*: string
   blend*: string
   alphaClip*: bool
   x*, y*: int32
+  id*: string
   
   case kind*: KritaLayerKind
   of klImage:
@@ -24,8 +26,10 @@ type KritaLayer* = ref object
     data*: seq[uint8]
   of klGroup:
     children*: seq[KritaLayer]
-  of klClone: #not really supported yet
-    cloneName*: string
+  of klClone:
+    #layer that is being cloned
+    clone*: KritaLayer
+    cloneId*: string
 
 type KritaDocument* = ref object
   width*, height*: int32
@@ -132,20 +136,16 @@ proc readKritaFile*(path: string): KritaDocument =
   
   let folderName = image.attr("name")
   
+  var
+    clonesToResolve: seq[KritaLayer]
+    idToLayer: Table[string, KritaLayer]
+  
   proc parseLayer(node: XmlNode): KritaLayer =
     
     let
       ntype = node.attr("nodetype")
-      opacity = if node.attr("opacity") == "": 1f else: parseInt(node.attr("opacity")).float32 / 255f
-      visible = node.attr("visible") == "1"
-      collapsed = node.attr("collapsed") == "1"
-      locked = node.attr("locked") == "1"
-      passthrough = node.attr("passthrough") == "1"
-      name = node.attr("name")
-      filename = node.attr("filename")
-      blend = node.attr("compositeop")
       channelFlags = node.attr("channelflags")
-      alphaClip = channelFlags.len > 0 and channelFlags[^1] == '0'
+      filename = node.attr("filename")
     
     if ntype == "grouplayer":
       var layers: seq[KritaLayer]
@@ -154,7 +154,7 @@ proc readKritaFile*(path: string): KritaDocument =
         let res = parseLayer(child)
         if res != nil: layers.add(res)
         
-      return KritaLayer(kind: klGroup, children: layers, opacity: opacity, visible: visible, passthrough: passthrough, name: name, blend: blend, locked: locked, alphaClip: alphaClip)
+      result = KritaLayer(kind: klGroup, children: layers)
     elif ntype == "paintlayer":
       let
         layerPath = folderName & "/layers/" & filename
@@ -162,15 +162,37 @@ proc readKritaFile*(path: string): KritaDocument =
         y = node.attr("y").parseInt.int32
         data = readImageData(archive.extractFile(layerPath), x, y, docWidth, docHeight)
       
-      return KritaLayer(kind: klImage, data: data, width: docWidth, height: docHeight, opacity: opacity, visible: visible, passthrough: passthrough, name: name, blend: blend, locked: locked, alphaClip: alphaClip)
+      result = KritaLayer(kind: klImage, data: data, width: docWidth, height: docHeight)
     elif ntype == "clonelayer":
         let
-          target = node.attr("clonefrom")
+          cloneId = node.attr("clonefromuuid")
           x = node.attr("x").parseInt.int32
           y = node.attr("y").parseInt.int32
         
-        return KritaLayer(kind: klClone, cloneName: target, x: x, y: y, opacity: opacity, visible: visible, passthrough: passthrough, name: name, blend: blend, locked: locked, alphaClip: alphaClip)
+        result = KritaLayer(kind: klClone, cloneId: cloneId, x: x, y: y)
+        #resolve clone target based on ID later
+        clonesToResolve.add result
+    
+    if result != nil:
+      result.opacity = if node.attr("opacity") == "": 1f else: parseInt(node.attr("opacity")).float32 / 255f
+      result.visible = node.attr("visible") == "1"
+      result.collapsed = node.attr("collapsed") == "1"
+      result.locked = node.attr("locked") == "1"
+      result.passthrough = node.attr("passthrough") == "1"
+      result.name = node.attr("name")
+      result.blend = node.attr("compositeop")
+      result.alphaClip = channelFlags.len > 0 and channelFlags[^1] == '0'
+      result.id = node.attr("uuid")
+      
+      idToLayer[result.id] = result
     
   for layer in layers:
     let res = parseLayer(layer)
-    if res != nil: result.layers.add(res)
+    if res != nil:
+      result.layers.add(res)
+  
+  for layer in clonesToResolve:
+    if layer.kind == klClone:
+      layer.clone = idToLayer[layer.cloneId]
+      if layer.clone == nil:
+        echo "Unresolved clone ID! name=", layer.name, " targetid=", layer.cloneId
